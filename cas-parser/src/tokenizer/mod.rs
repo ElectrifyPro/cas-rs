8f48dc9398631585735e9d0c769b425ab37ac6de
Tam Pham <tam001p@gmail.com>
2023-04-24 17:44:57 -0500

    Slight refactor, add tokenize_complete

---
+++ cas-parser/src/tokenizer/mod.rs
@@ -0,0 +1,84 @@
+pub mod token;
+
+use logos::{Lexer, Logos};
+pub use token::{Token, TokenKind};
+
+/// Returns an iterator over the token kinds produced by the tokenizer.
+pub fn tokenize(input: &str) -> Lexer<TokenKind> {
+    TokenKind::lexer(input)
+}
+
+/// Returns an owned array containing all of the tokens produced by the tokenizer. This allows us
+/// to backtrack in case of an error.
+pub fn tokenize_complete(input: &str) -> Result<Box<[Token]>, ()> {
+    let mut lexer = tokenize(input);
+    let mut tokens = Vec::new();
+
+    while let Some(kind) = lexer.next() {
+        let kind = kind?;
+        tokens.push(Token {
+            span: lexer.span(),
+            kind,
+            lexeme: lexer.slice(),
+        });
+    }
+
+    Ok(tokens.into_boxed_slice())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Compares the tokens produced by the tokenizer to the raw expected tokens.
+    fn compare_tokens<'source, const N: usize>(input: &'source str, expected: [(TokenKind, &'source str); N]) {
+        let mut lexer = tokenize(input);
+
+        for (expected_kind, expected_lexeme) in expected.into_iter() {
+            assert_eq!(lexer.next(), Some(Ok(expected_kind)));
+            assert_eq!(lexer.slice(), expected_lexeme);
+        }
+
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn basic_expr() {
+        compare_tokens(
+            "1 + 2",
+            [
+                (TokenKind::Int, "1"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Add, "+"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Int, "2"),
+            ],
+        );
+    }
+
+    #[test]
+    fn complex_expr() {
+        compare_tokens(
+            "3      x - 0xff + 0b101 * $",
+            [
+                (TokenKind::Int, "3"),
+                (TokenKind::Whitespace, "      "),
+                (TokenKind::Name, "x"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Sub, "-"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Hex, "0x"),
+                (TokenKind::Name, "ff"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Add, "+"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Bin, "0b"),
+                (TokenKind::Int, "101"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Mul, "*"),
+                (TokenKind::Whitespace, " "),
+                (TokenKind::Symbol, "$"),
+            ],
+        );
+    }
+}
---
+++ cas-parser/src/tokenizer/token.rs
@@ -0,0 +1,127 @@
+use logos::Logos;
+use std::ops::Range;
+
+/// The different kinds of tokens that can be produced by the tokenizer.
+#[derive(Logos, Clone, Copy, Debug, PartialEq)]
+pub enum TokenKind {
+    #[regex(r"[\n\r]+")]
+    NewLine,
+
+    #[regex(r"[ \t]+")]
+    Whitespace,
+
+    #[token("==")]
+    Eq,
+
+    #[token("!=")]
+    NotEq,
+
+    #[token("~==")]
+    ApproxEq,
+
+    #[token("~!=")]
+    ApproxNotEq,
+
+    #[token("+")]
+    Add,
+
+    #[token("-")]
+    Sub,
+
+    #[token("*")]
+    Mul,
+
+    #[token("/")]
+    Div,
+
+    #[token("%")]
+    Mod,
+
+    #[token("^")]
+    Exp,
+
+    #[token(">")]
+    Greater,
+
+    #[token(">=")]
+    GreaterEq,
+
+    #[token("<")]
+    Less,
+
+    #[token("<=")]
+    LessEq,
+
+    #[token("!")]
+    Bang, // used for both factorial and not operation
+
+    #[token("&&")]
+    And,
+
+    #[token("||")]
+    Or,
+
+    #[token("&")]
+    BitAnd,
+
+    #[token("|")]
+    BitOr,
+
+    #[token("~")]
+    BitNot,
+
+    #[token(">>")]
+    BitRight,
+
+    #[token("<<")]
+    BitLeft,
+
+    #[token("=")]
+    Assign,
+
+    #[token("0b")]
+    Bin,
+
+    #[token("0o")]
+    Oct,
+
+    #[token("0x")]
+    Hex,
+
+    #[regex(r"[a-zA-Z_]+")]
+    Name,
+
+    #[token(",")]
+    Comma,
+
+    #[token("(")]
+    OpenParen,
+
+    #[token(")")]
+    CloseParen,
+
+    #[regex(r"[0-9]+\.?")]
+    Int,
+
+    #[regex(r"[0-9]+\.[0-9]*")]
+    Float,
+
+    #[token(".")]
+    Dot,
+
+    #[regex(r".", priority = 0)]
+    Symbol,
+}
+
+/// A token produced by the tokenizer.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token<'source> {
+    /// The region of the source code that this token originated from.
+    pub span: Range<usize>,
+
+    /// The kind of token.
+    pub kind: TokenKind,
+
+    /// The raw lexeme that was parsed into this token.
+    pub lexeme: &'source str,
+}

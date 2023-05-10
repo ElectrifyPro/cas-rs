d66f097bdecaabbb70a2531ba70d36e86c812be7
Tam Pham <tam001p@gmail.com>
2023-05-10 15:49:58 -0500

    Add simple repl for testing

---
+++ cas-repl/src/main.rs
@@ -0,0 +1,22 @@
+use cas_eval::{ctxt::Ctxt, eval::Eval};
+use cas_parser::parser::{expr::Expr, Parser};
+use std::io;
+
+fn main() {
+    let ctxt = Ctxt::with_defaults();
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let expr = Parser::new(&input).try_parse_full::<Expr>();
+        match expr {
+            Ok(ast) => {
+                let result = ast.eval_with(&ctxt);
+                match result {
+                    Some(res) => println!("{}", res),
+                    None => eprintln!("Error: Could not evaluate expression"),
+                }
+            },
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
+}

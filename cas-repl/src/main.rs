ffc771eeebc71a5c88a536694bb586a176d55e0f
Tam Pham <tam001p@gmail.com>
2023-08-19 22:13:48 -0500

    Reset `ctxt` in cas-repl if evaluation fails

--- cas-repl/src/main.rs
+++ cas-repl/src/main.rs
@@ -1,20 +1,22 @@
 use ariadne::Source;
-use cas_eval::eval::Eval;
+use cas_eval::{ctxt::Ctxt, eval::Eval};
 use cas_parser::parser::{expr::Expr, Parser};
 use std::io;
 
 fn main() {
-    let mut ctxt = Default::default();
+    let mut ctxt = Ctxt::default();
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let expr = Parser::new(&input).try_parse_full::<Expr>();
         match expr {
             Ok(ast) => {
+                let ctxt_copy = ctxt.clone();
                 let result = ast.eval(&mut ctxt);
                 match result {
                     Ok(res) => println!("{}", res),
                     Err(e) => {
+                        ctxt = ctxt_copy;
                         let report = e.build_report();
                         report.eprint(("input", Source::from(input))).unwrap();
                     },

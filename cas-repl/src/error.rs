b32787bf314c039d1a48b92578e99f5d5acee446
Tam Pham <tam001p@gmail.com>
2023-10-04 22:56:15 -0500

    Support passing in filename to evaluate as argument to binary

---
+++ cas-repl/src/error.rs
@@ -0,0 +1,43 @@
+use ariadne::Source;
+use cas_eval::error::Error as EvalError;
+use cas_parser::parser::error::Error as ParseError;
+
+/// Utility enum to package errors that can occur while parsing / evaluating.
+pub enum Error {
+    /// Errors that occurred while parsing.
+    ParseError(Vec<ParseError>),
+
+    /// An error that occurred while evaluating.
+    EvalError(EvalError),
+}
+
+impl Error {
+    /// Report the errors in this [`Error`] to stderr.
+    ///
+    /// The `ariadne` crate's [`Report`] type actually does not have a `Display` implementation, so
+    /// we can only use its `eprint` method to print to stderr.
+    pub fn report_to_stderr(&self, input: &str) {
+        match self {
+            Self::ParseError(errs) => errs.iter().for_each(|err| {
+                let report = err.build_report();
+                report.eprint(("input", Source::from(input))).unwrap();
+            }),
+            Self::EvalError(err) => std::iter::once(err).for_each(|err| {
+                let report = err.build_report();
+                report.eprint(("input", Source::from(input))).unwrap();
+            }),
+        }
+    }
+}
+
+impl From<Vec<ParseError>> for Error {
+    fn from(errs: Vec<ParseError>) -> Self {
+        Self::ParseError(errs)
+    }
+}
+
+impl From<EvalError> for Error {
+    fn from(err: EvalError) -> Self {
+        Self::EvalError(err)
+    }
+}
--- cas-repl/src/main.rs
+++ cas-repl/src/main.rs
@@ -1,48 +1,54 @@
-use ariadne::Source;
-use cas_eval::{ctxt::Ctxt, eval::eval_stmts};
+mod error;
+
+use cas_eval::{ctxt::Ctxt, eval::eval_stmts, value::Value};
 use cas_parser::parser::{ast::stmt::Stmt, Parser};
-use std::io::{self, IsTerminal, Read, Write};
+use error::Error;
+use std::{fs::File, io::{self, BufReader, IsTerminal, Read, Write}};
+
+/// Parses and evaluates the given input string, returning the results of both operations.
+fn parse_eval(input: &str, ctxt: &mut Ctxt) -> Result<Value, Error> {
+    let ast = Parser::new(input).try_parse_full_many::<Stmt>()?;
+    let res = eval_stmts(&ast, ctxt)?;
+    Ok(res)
+}
+
+/// Reads from the provided file or stdin and parses / evaluates the input, printing the success or
+/// failure.
+fn read_eval(input: &str, ctxt: &mut Ctxt) {
+    match parse_eval(input, ctxt) {
+        Ok(res) => println!("{}", res),
+        Err(err) => err.report_to_stderr(input),
+    }
+}
 
 fn main() {
+    let mut args = std::env::args();
+    args.next();
+
     let mut ctxt = Ctxt::default();
-    loop {
+
+    if let Some(filename) = args.next() {
+        // run source file
+        let mut file = BufReader::new(File::open(filename).unwrap());
         let mut input = String::new();
-        let expr = if io::stdin().is_terminal() {
+        file.read_to_string(&mut input).unwrap();
+
+        read_eval(&input, &mut ctxt);
+    } else if !io::stdin().is_terminal() {
+        // read source from stdin
+        let mut input = String::new();
+        io::stdin().read_to_string(&mut input).unwrap();
+
+        read_eval(&input, &mut ctxt);
+    } else {
+        // run the repl / interactive mode
+        loop {
             print!("> ");
             io::stdout().flush().unwrap();
+            let mut input = String::new();
             io::stdin().read_line(&mut input).unwrap();
 
-            if input.trim().is_empty() {
-                continue;
-            }
-
-            Parser::new(&input).try_parse_full::<Stmt>()
-                .map(|stmt| vec![stmt])
-        } else {
-            io::stdin().read_to_string(&mut input).unwrap();
-            Parser::new(&input).try_parse_full_many::<Stmt>()
-        };
-
-        match expr {
-            Ok(ast) => {
-                match eval_stmts(&ast, &mut ctxt) {
-                    Ok(res) => println!("{}", res),
-                    Err(e) => {
-                        let report = e.build_report();
-                        report.eprint(("input", Source::from(&input))).unwrap();
-                    },
-                }
-            },
-            Err(errs) => {
-                for err in errs {
-                    let report = err.build_report();
-                    report.eprint(("input", Source::from(&input))).unwrap();
-                }
-            },
-        }
-
-        if !io::stdin().is_terminal() {
-            break;
+            read_eval(&input, &mut ctxt);
         }
     }
 }

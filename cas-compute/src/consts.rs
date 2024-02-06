babcd8a79d0edbadf486d40110bb221529318333
Tam Pham <tam001p@gmail.com>
2024-02-05 21:42:31 -0600

    Use 1.exp() to define E constant

--- cas-compute/src/consts.rs
+++ cas-compute/src/consts.rs
@@ -3,7 +3,7 @@
 
 use once_cell::sync::Lazy;
 use rug::{Complex, Float};
-use super::primitive::{complex, float, float_from_str};
+use super::primitive::{complex, float};
 
 pub static ZERO: Lazy<Float> = Lazy::new(|| float(0));
 
@@ -19,11 +19,7 @@ pub static TEN: Lazy<Float> = Lazy::new(|| float(10));
 pub static I: Lazy<Complex> = Lazy::new(|| complex((0, 1)));
 
 /// Euler's number.
-pub static E: Lazy<Float> = Lazy::new(|| {
-    float_from_str(
-        "2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746639193200305992181741359662904357290033429526059563",
-    )
-});
+pub static E: Lazy<Float> = Lazy::new(|| float(1).exp());
 
 /// The golden ratio.
 pub static PHI: Lazy<Float> = Lazy::new(|| (float(1) + float(5).sqrt()) / float(2));

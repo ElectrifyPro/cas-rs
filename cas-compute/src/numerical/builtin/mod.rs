55039ecd8250603da92034738e93661cb477e810
Tam Pham <tam001p@gmail.com>
2024-03-22 16:14:07 -0500

    Require Send Sync on Builtin

--- cas-compute/src/numerical/builtin/mod.rs
+++ cas-compute/src/numerical/builtin/mod.rs
@@ -7,7 +7,7 @@ use super::{ctxt::Ctxt, value::Value};
 type Result = std::result::Result<Value, BuiltinError>;
 
 /// A trait implemented by all builtin functions.
-pub trait Builtin: std::fmt::Debug {
+pub trait Builtin: std::fmt::Debug + Send + Sync {
     /// Number of args the function takes.
     // NOTE: this is a `&self` method and not an associated constant to make the trait object-safe
     fn num_args(&self) -> usize;

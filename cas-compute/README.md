38dd3ce07585dbd29bd93bfbfac985f4ad1ef639
Tam Pham <tam001p@gmail.com>
2025-05-19 12:58:04 -0500

    Fix failing doctests

--- cas-compute/README.md
+++ cas-compute/README.md
@@ -57,18 +57,18 @@ See its [module-level documentation](symbolic) for more information.
 
 ```rust
 use cas_compute::primitive::int;
-use cas_compute::symbolic::{expr::{Expr, Primary}, simplify};
-use cas_parser::parser::{ast::Expr as AstExpr, Parser};
+use cas_compute::symbolic::{expr::{SymExpr, Primary}, simplify};
+use cas_parser::parser::{ast::Expr, Parser};
 
 let mut parser = Parser::new("x + x + x");
-let ast_expr = parser.try_parse_full::<AstExpr>().unwrap();
+let ast_expr = parser.try_parse_full::<Expr>().unwrap();
 
 let simplified = simplify(&ast_expr.into());
 
 // `x + x + x = 3x`
-assert_eq!(simplified, Expr::Mul(vec![
-    Expr::Primary(Primary::Integer(int(3))),
-    Expr::Primary(Primary::Symbol("x".to_string())),
+assert_eq!(simplified, SymExpr::Mul(vec![
+    SymExpr::Primary(Primary::Integer(int(3))),
+    SymExpr::Primary(Primary::Symbol("x".to_string())),
 ]));
 ```
 
--- cas-compute/src/symbolic/mod.rs
+++ cas-compute/src/symbolic/mod.rs
@@ -28,7 +28,7 @@
 //! let mut parser = Parser::new("x + (y + z)");
 //! let ast_expr = parser.try_parse_full::<Expr>().unwrap();
 //!
-//! let expr = ast_expr.into();
+//! let expr: SymExpr = ast_expr.into();
 //! assert_eq!(expr, SymExpr::Add(vec![
 //!     SymExpr::Primary(Primary::Symbol("x".to_string())),
 //!     SymExpr::Primary(Primary::Symbol("y".to_string())),
@@ -57,17 +57,17 @@
 //!
 //! ```
 //! use cas_compute::primitive::int;
-//! use cas_compute::symbolic::{expr::{Expr, Primary}, simplify};
-//! use cas_parser::parser::{ast::Expr as AstExpr, Parser};
+//! use cas_compute::symbolic::{expr::{Primary, SymExpr}, simplify};
+//! use cas_parser::parser::{ast::Expr, Parser};
 //!
 //! let mut parser = Parser::new("x + x + x");
-//! let ast_expr = parser.try_parse_full::<AstExpr>().unwrap();
+//! let ast_expr = parser.try_parse_full::<Expr>().unwrap();
 //! let simplified = simplify(&ast_expr.into());
 //!
 //! // `x + x + x = 3x`
-//! assert_eq!(simplified, Expr::Mul(vec![
-//!     Expr::Primary(Primary::Integer(int(3))),
-//!     Expr::Primary(Primary::Symbol("x".to_string())),
+//! assert_eq!(simplified, SymExpr::Mul(vec![
+//!     SymExpr::Primary(Primary::Integer(int(3))),
+//!     SymExpr::Primary(Primary::Symbol("x".to_string())),
 //! ]));
 //! ```
 //!
